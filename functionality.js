9e575c682770b332b635c7f7db8ad514ecea637b
Portia-arch <portia.mkhabela@umuzi.org>
2020-02-13 22:33:16 +0200

    Skadoosh

---
+++ functionality.js
@@ -0,0 +1,9 @@
+//show the menu
+
+function showMenu() {
+    var toggle = document.getElementById("mobileMenu");
+
+    if (toogle.style.height == "0px") {
+        toggle.style.height = "20px";
+    }
+}
