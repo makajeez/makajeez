cd38afac2ccf1c49512b1b1636d0a6a7c96449f0
Portia-arch <portia.mkhabela@umuzi.org>
2020-02-27 11:35:03 +0200

    media queries

--- functionality.js
+++ functionality.js
@@ -1,9 +1,18 @@
 //show the menu
 
-function showMenu() {
-    var toggle = document.getElementById("mobileMenu");
+// function showMenu() {
+//     var toggle = document.getElementById("mobileMenu");
 
-    if (toogle.style.height == "0px") {
-        toggle.style.height = "20px";
+//     if (toogle.style.height == "0px") {
+//         toggle.style.height = "20px";
+//     }
+// }
+
+function myFunction() {
+    var x = document.getElementByClassName("nav-links");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
     }
 }
