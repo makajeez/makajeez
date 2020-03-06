31bea1814d0eb1a46aeeab841164c412df53461d
Portia-arch <portia.mkhabela@umuzi.org>
2020-03-06 12:01:18 +0200

    Kiii

--- functionality.js
+++ functionality.js
@@ -8,6 +8,18 @@
 //     }
 // }
 
+(document).ready(function () {
+    ('.menu-toggler').on('click', function () {
+        (this).toggleClass('open');
+        ('.top-nav').toggleClass('open');
+    });
+
+    ('.top-nav .nav-link').on('click', function () {
+        ('menu-toggler').removeClass('open');
+        ('.top-nav').removeClass('open');
+    });
+})
+
 function myFunction() {
     var x = document.getElementByClassName("nav-links");
     if (x.style.display === "block") {
@@ -15,4 +27,14 @@ function myFunction() {
     } else {
         x.style.display = "block";
     }
-}
+};
+
+let loader = document.getElementByClassName('loader');
+
+window.addEventListener('load', function(){
+    loader.style.height= "100px";
+    loader.style.width= "100px";
+    loader.style.borderRadius = "50%";
+    loader.style.visibility = "hidden";
+
+});
